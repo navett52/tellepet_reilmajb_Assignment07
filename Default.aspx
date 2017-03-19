586f280a81886b2040a0c3d331f012568791aba1
navett52 <evantellep@yahoo.com>
2017-03-19 18:37:40 -0400

    Adding back previous progress.

---
+++ Default.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/tellepet_reilmajb_Assignment07/Assn07Master.master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="tellepet_reilmajb_Assignment07_Default" %>
+
+
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="cphBody" Runat="Server">
+    <div>
+        <asp:Calendar ID="calStartDate" runat="server"></asp:Calendar>
+        <asp:Calendar ID="calEndDate" runat="server"></asp:Calendar>
+        <asp:TextBox ID="txtMinQty" runat="server"></asp:TextBox>
+        <asp:TextBox ID="txtMaxQty" runat="server"></asp:TextBox>
+        <asp:DropDownList ID="ddProducts" runat="server"></asp:DropDownList>
+        <asp:CheckBoxList ID="cblStores" runat="server"></asp:CheckBoxList>
+        <asp:Button ID="btnGenerate" runat="server" Text="Generate" OnClick="btnGenerate_Click" />
+    </div>
+</asp:Content>
+
---
+++ Default.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class tellepet_reilmajb_Assignment07_Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            tProductTableAdapter productTypeAdapter = new tProductTableAdapter();
+
+            //Populating the Transaction Type drop down using the databse.
+            ds_Products.tProductDataTable products = productTypeAdapter.GetData();
+            ddProducts.DataTextField = "TransactionType";
+            ddProducts.DataValueField = "TransactionTypeID";
+            ddProducts.DataSource = products;
+            ddProducts.DataBind();
+        }
+    }
+
+    protected void btnGenerate_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected string buildQuery(DateTime startDate, DateTime endDate, int minQty, int maxQty, List<string> stores)
+    {
+        string query = "";
+
+        
+
+        return query;
+    }
+}
