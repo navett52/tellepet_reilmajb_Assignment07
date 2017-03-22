914bbaebb4ad6bf1b84d1da78e5ebba40f3b7339
navett52 <evantellep@yahoo.com>
2017-03-22 00:29:49 -0400

    Building the report page

--- Report.aspx.cs
+++ Report.aspx.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 
 public partial class Report : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        HtmlGenericControl reportCell = new HtmlGenericControl();
+        reportCell.TagName = "div";
+        reportCell.Attributes["class"] = "reportCell";
 
+        foreach ()
+        {
+
+        }
     }
 }
